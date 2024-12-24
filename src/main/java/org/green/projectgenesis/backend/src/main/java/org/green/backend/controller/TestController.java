c4352cd9f6ba3263405d0bd16b0811f17029fd23
An Hyepin <gpqls9524@naver.com>
2024-12-24 19:14:09 +0900

    GD

--- src/main/java/org/green/projectgenesis/backend/src/main/java/org/green/backend/controller/TestController.java
+++ src/main/java/org/green/projectgenesis/backend/src/main/java/org/green/backend/controller/TestController.java
@@ -2,8 +2,9 @@ package org.green.backend.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
 }
