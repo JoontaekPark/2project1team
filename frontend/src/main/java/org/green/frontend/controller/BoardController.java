7347857f2fc1891b89e520f489da30219984aa78
Park seon young <qkrtjsdud608@naver.com>
2024-12-30 17:54:27 +0900

    1230

---
+++ frontend/src/main/java/org/green/frontend/controller/BoardController.java
@@ -0,0 +1,24 @@
+package org.green.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName : org.green.frontend.controller
+ * author : BoardController
+ * date : 2024-12-30
+ * description :
+ * ==========================================
+ * DATE             AUTHOR         NOTE
+ * ------------------------------------------
+ * 2024-12-30          GGG        최초생성
+ */
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/boardForm")
+    public String boardFormRoot() {
+        return "/board/boardForm";
+    }
+}
