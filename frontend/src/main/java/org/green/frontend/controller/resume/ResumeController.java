a0bd6f79328f5dca37a20e58bf8e2dcd99bcb224
JoontaekPark <klk510a@gmail.com>
2025-01-02 23:06:40 +0900

    2025_01/02_박준택_커밋 임시 이력서리스트 폼 작성...수정필수

--- frontend/src/main/java/org/green/frontend/controller/resume/ResumeController.java
+++ frontend/src/main/java/org/green/frontend/controller/resume/ResumeController.java
@@ -13,6 +13,7 @@ import org.green.frontend.utils.WebClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -127,6 +128,11 @@ import java.util.List;
   System.out.println("FrontEnd ResumeController info : " + resumeInfo);
   model.addAttribute("resume", resumeInfo);
   return "/resume/resumeDetail";
+ }
+
+ @GetMapping("/resumeList")
+ public String resumeList(){
+  return "/resume/resumeList";
  }
 
 
